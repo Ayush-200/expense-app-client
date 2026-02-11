0adb90174cc66424da7996fb02a7a29b9b32a1cf
Ayush Bhatia <ayushbhatia456@gmail.com>
2026-02-11 12:18:07 +0530

    added user model for authentication

---
+++ expense-server/src/model/users.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true},
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: false },
+    googleId: { type: String, required: false },
+    role: { type: String, enum: ['user', 'superadmin'], default: 'user' }
+});
+
+module.exports = mongoose.model('User', userSchema);
